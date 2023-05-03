9448beb699bdc2fd5de4a8dad374e66e83245c9f
pallavi1916 <rautpallavi1916@gmail.com>
2023-05-03 12:13:57 +0530

    First Change Commit

--- gateway/api/models.py
+++ gateway/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Coffee(models.Model):
-    name=models.CharField(max_length=200)
+    name=models.CharField(max_length=100)
     amount=models.CharField(max_length=200)
-    email=models.EmailField(max_length=200)
+    email=models.EmailField(max_length=100)
     payment_id=models.CharField(max_length=100)
     paid=models.BooleanField(default=False)
